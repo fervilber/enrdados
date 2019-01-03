6314bd32de1bc90a29937a92cbb465b8a581e6d6
fervilber <fvillalba@acuamed.es>
2019-01-03 15:59:13 +0100

    p21

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,25 +1,15 @@
-'use strict';
+const toggle = document.getElementById('toggle');
+const menu = document.getElementById('menu');
 
-(function iifeMenu(document, window, undefined) {
-	var menuBtn = document.querySelector('.menu__btn');
-	var	menu = document.querySelector('.menu__list');
+function toggleMenu() {
+	menu.classList.toggle('main-nav__list--active');
+	this.classList.toggle('main-nav__btn--active');
+	this.setAttribute(
+		'aria-expanded',
+		this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true',
+	);
+}
 
-	function toggleMenu() {
-		menu.classList.toggle('menu__list--active');
-		menu.classList.toggle('menu__list--transition');
-		this.classList.toggle('menu__btn--active');
-		this.setAttribute(
-			'aria-expanded',
-			this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
-		);
-	}
-
-	function removeMenuTransition() {
-		this.classList.remove('menu__list--transition');
-	}
-
-	if (menuBtn && menu) {
-		menuBtn.addEventListener('click', toggleMenu, false);
-		menu.addEventListener('transitionend', removeMenuTransition, false);
-	}
-}(document, window));
+if (toggle && menu) {
+	toggle.addEventListener('click', toggleMenu, false);
+}
